c9a11fd3a01dcb7f725f5d93fc4201fae2fc8f00
darvid7 <bvdarvid@gmail.com>
2017-03-17 22:26:48 +1100

    update stuff

--- utils/accum.py
+++ utils/accum.py
@@ -1,5 +1,5 @@
 import os
-csv_src_dir = ""
+csv_src_dir = "/Users/David/Desktop/unearthed-2017/unearthed-monash/unearthed-monash/Data/PI"
 
 # Create aggregated csv file.
 with open(os.path.join(csv_src_dir, "accum.csv"), 'w') as fh:
@@ -7,12 +7,17 @@ with open(os.path.join(csv_src_dir, "accum.csv"), 'w') as fh:
 
 files = os.listdir(csv_src_dir)
 
+count = 1
 with open(os.path.join(csv_src_dir, "accum.csv"), "a") as acc_fh:
 	for filename in files:
-        if filename = "accum.csv":
-                continue
-        with open(os.path.join(csv_src_dir, filename), 'a') as fh:
-            data = fh.read()
-            acc_fh.write(data)
-            acc_fh.write(["\n"])
+		if filename == "accum.csv":
+			continue
+		if not filename.endswith('.csv'):
+			continue
+		with open(os.path.join(csv_src_dir, filename), 'r') as fh:
+			data = fh.read()
+			data = ','.join([str(count), data])
+			acc_fh.write(data)
+			acc_fh.write("\n")
+			count += 1
 	                
