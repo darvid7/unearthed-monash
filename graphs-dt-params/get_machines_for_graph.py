835934d726c0a6721430a96abd35fa3e780efe48
darvid7 <bvdarvid@gmail.com>
2017-03-19 14:02:54 +1100

    update

---
+++ graphs-dt-params/get_machines_for_graph.py
@@ -0,0 +1,15 @@
+"""
+@author: David Lei
+@since: 19/03/2017
+@modified: 
+
+"""
+class GraphMachineComponents:
+    def __init__(self, code, name, dt_lt_eq_value, samples):
+        self.code = code
+        self.name = name
+        self.dt_lt_eq_value = dt_lt_eq_value
+        self.samples = samples
+
+DT_RELEVANT_MACHINE_COMPONENTS = " "
+
